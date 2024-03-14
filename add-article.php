42129d1962c110e111e92a1daf9eda6830dbdbc6
Marcus <marcus.frontdev@gmail.com>
2024-03-14 17:52:00 +0100

    implementation of local error and data management

--- add-article.php
+++ add-article.php
@@ -1,3 +1,75 @@
+<?php 
+    const ERROR_REQUIRED='Veuillez renseigner ce champ';
+    const ERROR_TITLE_TOO_SHORT= 'Le titre est trop court';
+    const ERROR_CONTENT_TOO_SHORT= 'L\'article est trop court';
+    const ERROR_IMAGE_URL='l\'image doit être une url valide';
+    $filename=__DIR__ . '/data/articles.json';
+    $errors=[
+        'title' => '',
+        'image' =>'',
+        'category' => '',
+        'content' => ''
+    ];
+
+    $articles=[];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if(file_exists($filename)){
+            $articles = json_decode(file_get_contents($filename),true) ?? [];
+        }
+        $_POST = filter_input_array(INPUT_POST, [
+            'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'image' =>FILTER_SANITIZE_URL,
+            'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'content' => [
+                'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+                'flags' => FILTER_FLAG_NO_ENCODE_QUOTES
+            ]
+        ]);
+        $title=$_POST['title'] ?? '';
+        $image=$_POST['image'] ?? '';
+        $category=$_POST['category'] ?? '';
+        $content=$_POST['content'] ?? '';
+
+        if(!$title){
+            $errors['title'] = ERROR_REQUIRED;
+        }elseif(mb_strlen($title) < 5){
+            $errors['title'] = ERROR_TITLE_TOO_SHORT;
+        }
+
+        if(!$image){
+            $errors['image'] = ERROR_REQUIRED;
+        }elseif(!filter_var($image, FILTER_VALIDATE_URL)){
+            $errors['image'] = ERROR_IMAGE_URL;
+        }
+
+        if(!$category){
+            $errors['category'] = ERROR_REQUIRED;
+        }
+
+        if(!$content){
+            $errors['content'] = ERROR_REQUIRED;
+        }elseif(mb_strlen($content) < 50){
+            $errors['content'] = ERROR_CONTENT_TOO_SHORT;
+        }
+        
+
+        if(empty(array_filter($errors, fn($e)=> $e!==''))){
+          $articles=[...$articles,[
+            'title' => $title,
+            'image' => $image,
+            'category' => $category,
+            'content'=> $content,
+            'id' => time()
+          ]];
+          file_put_contents($filename, json_encode($articles));
+          header('Location:/');
+        }
+ 
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -16,31 +88,39 @@
             <form action="/add-article.php" method="POST">
                 <div class="form-control">
                     <label for="title">Titre</label>
-                    <input type="text" name="title" id="title">
-                    <!-- <p class="text-error"></p> -->
+                    <input type="text" name="title" id="title" value=<?= $title ??'' ?>>
+                    <?php if($errors['title']) : ?>
+                    <p class="text-error"><?=$errors['title']?></p>
+                    <?php endif;?>
                 </div>
                 <div class="form-control">
                     <label for="image">Image</label>
-                    <input type="text" name="image" id="image">
-                    <!-- <p class="text-error"></p> -->
+                    <input type="text" name="image" id="image" value=<?= $image ??'' ?>>
+                    <?php if($errors['image']) : ?>
+                    <p class="text-error"><?=$errors['image']?></p>
+                    <?php endif;?>
                 </div>
                 <div class="form-control">
                     <label for="category">Catégorie</label>
                     <select name="category" id="category">
                         <option value="technology">Technologie</option>
-                        <option value="technology">Nature</option>
-                        <option value="technology">Politique</option>
+                        <option value="nature">Nature</option>
+                        <option value="politics">Politique</option>
                     </select>
-                    <!-- <p class="text-error"></p> -->
+                    <?php if($errors['category']) : ?>
+                    <p class="text-error"><?=$errors['category']?></p>
+                    <?php endif;?>
                 </div>
                 <div class="form-control">
                     <label for="content">Contenu</label>
-                    <textarea name="content" id="content"></textarea>
-                    <!-- <p class="text-error"></p> -->
+                    <textarea name="content" id="content" value=<?= $content ??'' ?>></textarea>
+                    <?php if($errors['content']) : ?>
+                    <p class="text-error"><?=$errors['content']?></p>
+                    <?php endif;?>
                 </div>
                 <div class="form-action">
                     <a href="/" class="btn btn-secondary" type="button">Annuler</a>
-                    <button class="btn btn-primary" type="button">Sauvegarder</button>
+                    <button class="btn btn-primary" type="submit">Sauvegarder</button>
                 </div>
             </form>
         </div>
