a57ab10761aa386c270f25c044dde96bac611de2
Marcus <marcus.frontdev@gmail.com>
2024-03-14 15:02:23 +0100

    first commit setting up the header and style

---
+++ add-article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <?php require_once './components/head.php' ?>
+  <link rel="stylesheet" href="./public/css/add-article.css">
+  <title>Créer un article</title>
+</head>
+
+<body>
+  <div class="container">
+    <?php require_once './components/header.php' ?>
+    <div class="content">
+    </div>
+    <?php require_once './components/footer.php' ?>
+  </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <?php require_once './components/head.php' ?>
+  <link rel="stylesheet" href="./public/css/index.css">
+  <title>Blog</title>
+</head>
+
+<body>
+  <div class="container">
+    <?php require_once './components/header.php' ?>
+    <div class="content">
+    </div>
+    <?php require_once './components/footer.php' ?>
+  </div>
+</body>
+
+</html>
